ee39b9c75fcb5e162e59cd6b620924fc66e9f231
root <root@chaos.home>
2019-02-19 12:26:29 +1000

    update banner

--- swalow.py
+++ swalow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
             ###############
-              S.W.A.L.O.W 
+            # S.W.A.L.O.W #
             ###############
        
    # Simple Web App Load Overload Weapon #
